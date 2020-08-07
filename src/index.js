3cabdfc0b8110c7b5bbbd3000ea17a62ccac9f0c
AposVinc <apos.vincenzo@gmail.com>
2020-08-07 19:00:05 +0200

    parametro parent nella funz. createElementWithClasses

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ window.addEventListener('load', function component() {
     }
   };
 
-  let createElementWithClasses = function (tag, classes = []){
+  let createElementWithClasses = function (tag, classes = [], parent = null){
     let element = document.createElement(tag);
     classes.forEach( c => element.classList.add(c));
+    if (parent !== null){
+      parent.appendChild(element);
+    }
     return element;
   };
 
@@ -18,27 +21,23 @@ window.addEventListener('load', function component() {
   app.id = 'app';
   document.body.appendChild(app);
 
-  let header = createElementWithClasses('header', ['w3-container', 'w3-theme']);
+  let header = createElementWithClasses('header', ['w3-container', 'w3-theme'], app);
   setStyle(header, {
     padding: '15px'
   });
 
-  let div = createElementWithClasses('div', ['w3-center']);
-  header.appendChild(div);
-  let h1 = createElementWithClasses('h1', ['w3-xxxlarge', 'w3-animate-bottom']);
+  let div_w3center = createElementWithClasses('div', ['w3-center'], header);
+  let h1 = createElementWithClasses('h1', ['w3-xxxlarge', 'w3-animate-bottom'],div_w3center);
   h1.innerHTML = 'MemoJS';
-  div.appendChild(h1);
 
-  app.appendChild(header);
-
-  let container = createElementWithClasses('div', ['w3-container']);
+  let container = createElementWithClasses('div', ['w3-container'],app);
   setStyle(container, {
     backgroundColor: '#d1d1d1'
-  })
-  app.appendChild(container);
+  });
 
-  let row = createElementWithClasses('div', ['w3-row-padding', 'w3-center,', 'w3-margin-top']);
-  container.appendChild(row);
+  let row = createElementWithClasses('div', ['w3-row-padding', 'w3-center,', 'w3-margin-top'],container);
+
+
 
 
 
